019359c7b99c9d68c5b3b8d8e640109f6b26eeb5
Philozoraptor <philozoraptor@gmail.com>
2022-01-30 00:08:42 +0100

    roll back

--- RealisticBattleCombatModule/CombatModule/DamageRework.cs
+++ RealisticBattleCombatModule/CombatModule/DamageRework.cs
@@ -403,13 +403,14 @@ namespace RealisticBattleCombatModule
                     weaponWeight += 0.5f;
                 }
                 float kineticEnergy = 0.5f * weaponWeight * combinedSpeed * combinedSpeed;
-                float mixedEnergy = 0.5f *  (weaponWeight + 1.5f) * combinedSpeed * combinedSpeed;
+                float basedamage = 138f + kineticEnergy / 16;
+                //float mixedEnergy = 0.5f *  (weaponWeight + 1.5f) * combinedSpeed * combinedSpeed;
                 //float basedamage = 0.5f * (weaponWeight + 4.5f) * combinedSpeed * combinedSpeed;
-                float basedamage = 120f;
-                if (mixedEnergy > 120f)
-                {
-                    basedamage = mixedEnergy;
-                }
+                //float basedamage = 138f;
+                //if (mixedEnergy > 138f)
+                //{
+                //    basedamage = mixedEnergy;
+                //}
                 //float handBonus = 0.5f * (weaponWeight + 1.5f) * combinedSpeed * combinedSpeed;
                 //float handLimit = 120f;
                 //if (handBonus > handLimit)
@@ -421,9 +422,9 @@ namespace RealisticBattleCombatModule
                 //{
                 //    thrust = kineticEnergy;
                 //}
-                else if (basedamage > 180f)
+                if (basedamage > 150f)
                 {
-                    basedamage = 180f;
+                    basedamage = 150f;
                 }
                 float thrust = basedamage;
                 if (kineticEnergy > basedamage)
