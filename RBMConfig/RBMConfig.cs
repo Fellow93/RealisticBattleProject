0bf43d8ce901938c12fd8238798c2f4f12b19e19
Philozoraptor <philozoraptor@gmail.com>
2022-08-20 00:47:40 +0100

    horse polearm fix

--- RBMConfig/RBMConfig.cs
+++ RBMConfig/RBMConfig.cs
@@ -8,9 +8,9 @@ namespace RBMConfig
     public static class RBMConfig
     {
         public static XmlDocument xmlConfig = new XmlDocument();
-        public static float ThrustMagnitudeModifier = 0.025f;
-        public static float OneHandedThrustDamageBonus = 40f;
-        public static float TwoHandedThrustDamageBonus = 40f;
+        public static float ThrustMagnitudeModifier = 0.02f;
+        public static float OneHandedThrustDamageBonus = 50f;
+        public static float TwoHandedThrustDamageBonus = 50f;
         //modules
         public static bool rbmTournamentEnabled = true;
         public static bool rbmAiEnabled = true;
