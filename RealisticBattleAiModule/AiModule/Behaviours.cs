1e88e08c0066bf8d027d3797f20f73fc7a80f445
Marek Náhlik <marek.nahlik93@gmail.com>
2022-10-06 08:59:15 +0200

    Update Behaviours.cs

--- RealisticBattleAiModule/AiModule/Behaviours.cs
+++ RealisticBattleAiModule/AiModule/Behaviours.cs
@@ -1391,7 +1391,7 @@ namespace RBMAI
                 else
                 {
                     unit.SetAIBehaviorValues(AISimpleBehaviorKind.GoToPos, 3f, 7f, 5f, 20f, 6f);
-                    unit.SetAIBehaviorValues(AISimpleBehaviorKind.Melee, 8f, 4f, 3f, 20f, 0.01f);
+                    unit.SetAIBehaviorValues(AISimpleBehaviorKind.Melee, 8f, 4.5f, 3f, 20f, 0.01f);
                     unit.SetAIBehaviorValues(AISimpleBehaviorKind.Ranged, 0.02f, 7f, 0.04f, 20f, 0.03f);
                     unit.SetAIBehaviorValues(AISimpleBehaviorKind.ChargeHorseback, 10f, 7f, 5f, 30f, 0.05f);
                     unit.SetAIBehaviorValues(AISimpleBehaviorKind.RangedHorseback, 0.02f, 15f, 0.065f, 30f, 0.055f);
