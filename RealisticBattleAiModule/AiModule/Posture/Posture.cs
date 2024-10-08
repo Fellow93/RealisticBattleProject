65dde4be2ec70a1b11e6e9ef097d4769dba95c58
Philozoraptor <philozoraptor@gmail.com>
2024-10-08 01:05:53 +0100

    experiment (swords)

--- RealisticBattleAiModule/AiModule/Posture/Posture.cs
+++ RealisticBattleAiModule/AiModule/Posture/Posture.cs
@@ -7,7 +7,7 @@ namespace RBMAI
     {
         public float posture;
         public float maxPosture = 100f;
-        public float regenPerTick = 0.01f;
+        public float regenPerTick = 0.01f; // base 0.04 per tick (2.4 per second)
         public int maxPostureLossCount = 0;
         public float lastPostureLossTime = 0f;
 
@@ -42,65 +42,65 @@ namespace RBMAI
         public const float POSTURE_RESET_MODIFIER = 0.75f;
         public const float SHIELD_POSTURE_RESET_MODIFIER = 1f;
 
-        public const float ONEHANDEDSWORD_SWING_COST = 15f;
-        public const float ONEHANDEDSWORD_THRUST_COST = 15f;
-        public const float ONEHANDEDSWORD_OVERHEAD_COST = 15f;
-        public const float ONEHANDEDSWORD_SWING_DRAIN = 15f;
-        public const float ONEHANDEDSWORD_THRUST_DRAIN = 15f;
-        public const float ONEHANDEDSWORD_OVERHEAD_DRAIN = 15f;
-        public const float ONEHANDEDSWORD_BLOCK_COST = 15f;
-        public const float ONEHANDEDSWORD_PARRY_COST = 15f;
+        public const float ONEHANDEDSWORD_SWING_COST = 10f;
+        public const float ONEHANDEDSWORD_THRUST_COST = 8f;
+        public const float ONEHANDEDSWORD_OVERHEAD_COST = 12f;
+        public const float ONEHANDEDSWORD_SWING_DRAIN = 25f;
+        public const float ONEHANDEDSWORD_THRUST_DRAIN = 20f;
+        public const float ONEHANDEDSWORD_OVERHEAD_DRAIN = 30f;
+        public const float ONEHANDEDSWORD_BLOCK_COST = 0f;
+        public const float ONEHANDEDSWORD_PARRY_COST = -5f;
         public const float ONEHANDEDSWORD_HIT_COST = 15f;
-        public const float ONEHANDEDSWORD_BLOCK_REFLECT = 15f;
+        public const float ONEHANDEDSWORD_BLOCK_REFLECT = 5f;
         public const float ONEHANDEDSWORD_PARRY_REFLECT = 15f;
 
-        public const float DAGGER_SWING_COST = 15f;
-        public const float DAGGER_THRUST_COST = 15f;
-        public const float DAGGER_OVERHEAD_COST = 15f;
-        public const float DAGGER_SWING_DRAIN = 15f;
-        public const float DAGGER_THRUST_DRAIN = 15f;
-        public const float DAGGER_OVERHEAD_DRAIN = 15f;
+        public const float DAGGER_SWING_COST = 5f;
+        public const float DAGGER_THRUST_COST = 3f;
+        public const float DAGGER_OVERHEAD_COST = 7f;
+        public const float DAGGER_SWING_DRAIN = 10f;
+        public const float DAGGER_THRUST_DRAIN = 20f;
+        public const float DAGGER_OVERHEAD_DRAIN = 13f;
         public const float DAGGER_BLOCK_COST = 15f;
-        public const float DAGGER_PARRY_COST = 15f;
+        public const float DAGGER_PARRY_COST = 10f;
         public const float DAGGER_HIT_COST = 15f;
-        public const float DAGGER_BLOCK_REFLECT = 15f;
-        public const float DAGGER_PARRY_REFLECT = 15f;
+        public const float DAGGER_BLOCK_REFLECT = 0f;
+        public const float DAGGER_PARRY_REFLECT = 0f;
 
-        public const float TWOHANDEDSWORD_SWING_COST = 15f;
-        public const float TWOHANDEDSWORD_THRUST_COST = 15f;
-        public const float TWOHANDEDSWORD_OVERHEAD_COST = 15f;
-        public const float TWOHANDEDSWORD_SWING_DRAIN = 15f;
-        public const float TWOHANDEDSWORD_THRUST_DRAIN = 15f;
-        public const float TWOHANDEDSWORD_OVERHEAD_DRAIN = 15f;
-        public const float TWOHANDEDSWORD_BLOCK_COST = 15f;
-        public const float TWOHANDEDSWORD_PARRY_COST = 15f;
+        public const float TWOHANDEDSWORD_SWING_COST = 14f;
+        public const float TWOHANDEDSWORD_THRUST_COST = 11f;
+        public const float TWOHANDEDSWORD_OVERHEAD_COST = 17f;
+        public const float TWOHANDEDSWORD_SWING_DRAIN = 40f;
+        public const float TWOHANDEDSWORD_THRUST_DRAIN = 30f;
+        public const float TWOHANDEDSWORD_OVERHEAD_DRAIN = 50f;
+        public const float TWOHANDEDSWORD_BLOCK_COST = -5f;
+        public const float TWOHANDEDSWORD_PARRY_COST = -10f;
         public const float TWOHANDEDSWORD_HIT_COST = 15f;
-        public const float TWOHANDEDSWORD_BLOCK_REFLECT = 15f;
-        public const float TWOHANDEDSWORD_PARRY_REFLECT = 15f;
+        public const float TWOHANDEDSWORD_BLOCK_REFLECT = 10f;
+        public const float TWOHANDEDSWORD_PARRY_REFLECT = 30f;
 
-        public const float ONEHANDEDAXE_SWING_COST = 15f;
-        public const float ONEHANDEDAXE_THRUST_COST = 15f;
-        public const float ONEHANDEDAXE_OVERHEAD_COST = 15f;
-        public const float ONEHANDEDAXE_SWING_DRAIN = 15f;
-        public const float ONEHANDEDAXE_THRUST_DRAIN = 15f;
-        public const float ONEHANDEDAXE_OVERHEAD_DRAIN = 15f;
+        public const float ONEHANDEDAXE_SWING_COST = 12f;
+        public const float ONEHANDEDAXE_THRUST_COST = 10f;
+        public const float ONEHANDEDAXE_OVERHEAD_COST = 14f;
+        public const float ONEHANDEDAXE_SWING_DRAIN = 18f;
+        public const float ONEHANDEDAXE_THRUST_DRAIN = 14f;
+        public const float ONEHANDEDAXE_OVERHEAD_DRAIN = 22f;
         public const float ONEHANDEDAXE_BLOCK_COST = 15f;
-        public const float ONEHANDEDAXE_PARRY_COST = 15f;
+        public const float ONEHANDEDAXE_PARRY_COST = 7f;
         public const float ONEHANDEDAXE_HIT_COST = 15f;
-        public const float ONEHANDEDAXE_BLOCK_REFLECT = 15f;
-        public const float ONEHANDEDAXE_PARRY_REFLECT = 15f;
+        public const float ONEHANDEDAXE_BLOCK_REFLECT = 0f;
+        public const float ONEHANDEDAXE_PARRY_REFLECT = 17f;
 
-        public const float TWOHANDEDAXE_SWING_COST = 15f;
-        public const float TWOHANDEDAXE_THRUST_COST = 15f;
-        public const float TWOHANDEDAXE_OVERHEAD_COST = 15f;
-        public const float TWOHANDEDAXE_SWING_DRAIN = 15f;
-        public const float TWOHANDEDAXE_THRUST_DRAIN = 15f;
-        public const float TWOHANDEDAXE_OVERHEAD_DRAIN = 15f;
-        public const float TWOHANDEDAXE_BLOCK_COST = 15f;
-        public const float TWOHANDEDAXE_PARRY_COST = 15f;
+        public const float TWOHANDEDAXE_SWING_COST = 17f;
+        public const float TWOHANDEDAXE_THRUST_COST = 14f;
+        public const float TWOHANDEDAXE_OVERHEAD_COST = 20f;
+        public const float TWOHANDEDAXE_SWING_DRAIN = 36f;
+        public const float TWOHANDEDAXE_THRUST_DRAIN = 28f;
+        public const float TWOHANDEDAXE_OVERHEAD_DRAIN = 44f;
+        public const float TWOHANDEDAXE_BLOCK_COST = 10f;
+        public const float TWOHANDEDAXE_PARRY_COST = 4f;
         public const float TWOHANDEDAXE_HIT_COST = 15f;
-        public const float TWOHANDEDAXE_BLOCK_REFLECT = 15f;
-        public const float TWOHANDEDAXE_PARRY_REFLECT = 15f;
+        public const float TWOHANDEDAXE_BLOCK_REFLECT = 5f;
+        public const float TWOHANDEDAXE_PARRY_REFLECT = 34f;
 
         public const float MACE_SWING_COST = 15f;
         public const float MACE_THRUST_COST = 15f;
@@ -150,24 +150,24 @@ namespace RBMAI
         public const float TWOHANDEDPOLEARM_BLOCK_REFLECT = 15f;
         public const float TWOHANDEDPOLEARM_PARRY_REFLECT = 15f;
 
-        public const float SMALLSHIELD_INCORRECT_BLOCK_COST = 15f;
-        public const float SMALLSHIELD_BLOCK_COST = 15f;
-        public const float SMALLSHIELD_PARRY_COST = 15f;
+        public const float SMALLSHIELD_INCORRECT_BLOCK_COST = 0f;
+        public const float SMALLSHIELD_BLOCK_COST = -5f;
+        public const float SMALLSHIELD_PARRY_COST = -20f;
         public const float SMALLSHIELD_HIT_COST = 15f;
-        public const float SMALLSHIELD_INCORRECT_BLOCK_REFLECT = 15f;
-        public const float SMALLSHIELD_BLOCK_REFLECT = 15f;
+        public const float SMALLSHIELD_INCORRECT_BLOCK_REFLECT = 0f;
+        public const float SMALLSHIELD_BLOCK_REFLECT = 5f;
         public const float SMALLSHIELD_PARRY_REFLECT = 15f;
 
-        public const float LARGESHIELD_INCORRECT_BLOCK_COST = 15f;
-        public const float LARGESHIELD_BLOCK_COST = 15f;
-        public const float LARGESHIELD_PARRY_COST = 15f;
+        public const float LARGESHIELD_INCORRECT_BLOCK_COST = -5f;
+        public const float LARGESHIELD_BLOCK_COST = -10f;
+        public const float LARGESHIELD_PARRY_COST = -15f;
         public const float LARGESHIELD_HIT_COST = 15f;
-        public const float LARGESHIELD_INCORRECT_BLOCK_REFLECT = 15f;
-        public const float LARGESHIELD_BLOCK_REFLECT = 15f;
-        public const float LARGESHIELD_PARRY_REFLECT = 15f;
+        public const float LARGESHIELD_INCORRECT_BLOCK_REFLECT = 0f;
+        public const float LARGESHIELD_BLOCK_REFLECT = 0f;
+        public const float LARGESHIELD_PARRY_REFLECT = 5f;
 
         public const float SHIELD_ON_BACK_HIT_COST = 15f;
-        public const float SHIELD_ON_BACK_HIT_REFLECT = 15f;
+        public const float SHIELD_ON_BACK_HIT_REFLECT = 0f;
 
         public static string getWeaponClassString(WeaponClass wc)
         {
