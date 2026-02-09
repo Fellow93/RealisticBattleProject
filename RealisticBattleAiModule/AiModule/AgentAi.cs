f1bf81975d89f75d3d3a456cf6fbef651602f3ee
mnahlikjr <marek.nahlik93@gmail.com>
2026-02-09 21:20:02 +0100

    cav charge arena retreat fix

--- RealisticBattleAiModule/AiModule/AgentAi.cs
+++ RealisticBattleAiModule/AiModule/AgentAi.cs
@@ -667,7 +667,7 @@ namespace RBMAI
                     attacker.RiderAgent.EventControlFlags &= ~Agent.EventControlFlag.DoubleTapToDirectionMask;
                     attacker.RiderAgent.EventControlFlags |= Agent.EventControlFlag.DoubleTapToDirectionUp;
                 }
-                if (attacker.RiderAgent != null && attacker.IsEnemyOf(victim))
+                if (attacker.RiderAgent != null && Mission.Current != null && (Mission.Current.IsFieldBattle || Mission.Current.IsSallyOutBattle) && attacker.IsEnemyOf(victim))
                 {
                     bool isKnockDown = blow.BlowFlag.HasFlag(BlowFlags.KnockDown);
                     bool isKnockBack = blow.BlowFlag.HasFlag(BlowFlags.KnockBack);
