b041809a562b70488f99945cc77fe935bde28614
mnahlikjr <marek.nahlik93@gmail.com>
2026-02-26 19:32:12 +0100

    Update RBMTacticEmbolon.cs

--- RealisticBattleAiModule/AiModule/RbmTactics/RBMTacticEmbolon.cs
+++ RealisticBattleAiModule/AiModule/RbmTactics/RBMTacticEmbolon.cs
@@ -183,6 +183,7 @@ public class RBMTacticEmbolon : TacticComponent
                                     heavyCavCount++;
                                 }
                             }
+                            cavCount++;
                         }
                     });
                 }
